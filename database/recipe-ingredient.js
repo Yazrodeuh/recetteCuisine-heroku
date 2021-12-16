2f9436b5332c8c765a34992313c8a6ed40d0085c
maxime <max.poujol21@gmail.com>
2021-12-16 15:04:09 +0100

    request database

--- database/recipe-ingredient.js
+++ database/recipe-ingredient.js
@@ -13,7 +13,7 @@ function selectAll(req, res){
 
 function selectCompleteRecipe(req, res){
 
-    database.select(dbName + "?q={\"recipe-ingredient\":{\"recipe.id\":" + req.params.id + "}}", req, res);
+    database.select(dbName + "?q={\"recipe\":{\"id\":" + req.params.id + "}}", req, res);
 
 
 
