99b5c2600b7b2d3107b1cb223c7903d0a8f0d9a2
maxime <max.poujol21@gmail.com>
2021-12-16 10:35:17 +0100

    function requestDB

--- database/requestDatabase.js
+++ database/requestDatabase.js
@@ -1,10 +1,13 @@
 const axios = require("axios");
 
-
 async function requestDB(url){
     return await axios.get("https://recettecuisine-d83f.restdb.io/rest/" + url, {
         headers: {
             "x-apikey": process.env.XAPIKEY
         }
     })
+}
+
+module.exports = {
+    requestDB: requestDB
 }
