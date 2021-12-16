826df3615367a707ae00b1abba66559c3418730a
maxime <max.poujol21@gmail.com>
2021-12-16 10:15:14 +0100

    function requestDB

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000 // this is very important
 const axios = require("axios")
+
+
+
+
+
+
+
+
+
+
+async function requestDB(url){
+    return await axios.get("https://recettecuisine-d83f.restdb.io/rest/" + url, {
+        headers: {
+            "x-apikey": process.env.XAPIKEY
+        }
+    })
+}
