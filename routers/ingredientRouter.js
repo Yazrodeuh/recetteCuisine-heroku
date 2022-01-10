3cb292529fb4b44ab64ee591afd163503f19e8f1
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2022-01-10 12:07:30 +0100

    structure des routes

--- routers/ingredientRouter.js
+++ routers/ingredientRouter.js
@@ -3,9 +3,9 @@ const ingredient = require("../database/ingredient.js");
 const Authentification = require("../authentification/authentification");
 
 router.get('/', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.selectAll);
-router.get('/:id', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.selectOneById);
-router.post('/', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.createObj);
-router.put('/', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.updateObj);
-router.delete('/', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.deleteObj);
+// router.get('/:id', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.selectOneById);
+// router.post('/', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.createObj);
+// router.put('/', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.updateObj);
+// router.delete('/', Authentification.passeport.authenticate('jwt', {session: false}), ingredient.deleteObj);
 
 module.exports = router;
