4ce4983d70a0c76508c9a84961250d2ff9b12330
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2022-02-10 10:40:24 +0100

    maj router recipe

--- routers/recipeRouter.js
+++ routers/recipeRouter.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 const recipe = require("../database/recipe.js");
-const recipeIngredient = require("../database/recipe-ingredient.js");
+// const recipeIngredient = require("../database/recipe-ingredient.js");
 const Authentification = require("../authentification/authentification");
 
 
 
 router.get('/', Authentification.passeport.authenticate('jwt', {session: false}), recipe.selectAll);
-// router.get('/:id', Authentification.passeport.authenticate('jwt', {session: false}), recipe.selectOneById);
-// router.post('/', Authentification.passeport.authenticate('jwt', {session: false}), recipe.createObj);
-// router.put('/', Authentification.passeport.authenticate('jwt', {session: false}), recipe.updateObj);
-// router.delete('/', Authentification.passeport.authenticate('jwt', {session: false}), recipe.deleteObj);
+router.get('/:id', Authentification.passeport.authenticate('jwt', {session: false}), recipe.selectOneById);
+router.post('/', Authentification.passeport.authenticate('jwt', {session: false}), recipe.createObj);
+router.put('/', Authentification.passeport.authenticate('jwt', {session: false}), recipe.updateObj);
+router.delete('/', Authentification.passeport.authenticate('jwt', {session: false}), recipe.deleteObj);
 
 
 
 //router.get('/', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.selectAll);
-router.get('/:id', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.selectOneById);
-router.post('/', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.createObj);
-router.put('/', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.updateObj);
-router.delete('/', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.deleteObj);
+// router.get('/:id', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.selectOneById);
+// router.post('/', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.createObj);
+// router.put('/', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.updateObj);
+// router.delete('/', Authentification.passeport.authenticate('jwt', {session: false}), recipeIngredient.deleteObj);
 
 
 module.exports = router;
